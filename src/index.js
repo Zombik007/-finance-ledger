962e59eb59265d1d3cb14cc11f8aa00ab0accb2d
Vitaliy Myts <74954880+Zombik007@users.noreply.github.com>
2022-02-19 15:06:58 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,7 +16,6 @@ function changeHeaderBackground() {
 }
 
 let form = document.querySelector('.formWithValidation');
-let validateBtn = form.querySelector('.contact__send-button');
 let inputFieldEmail = document.querySelector('.js-contact__input-field');
 let inputWarning = document.querySelector('.contact__input-warning');
 
@@ -26,14 +25,13 @@ function hadlerSubmit(e) {
   e.preventDefault();
   if (inputFieldEmail.value.length === 0) {
     inputWarning.style.opacity = 1;
-    console.log('input empty');
 
     setTimeout(() => {
       inputWarning.style.opacity = 0;
     }, 5000);
     return;
   }
-  let form = document.querySelector('.formWithValidation');
+
   let formData = new FormData(form);
   fetch('/', {
     method: 'POST',
